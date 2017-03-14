6220d5ac85f723443e925d23ee16bd300843aa12
jschhzh <chenhongzhi@chenhongzhideMacBook-Pro.local>
2017-03-14 15:44:35 +0800

    add  physical backup restore

--- mysql/backup_recovery.py
+++ mysql/backup_recovery.py
@@ -8,8 +8,9 @@
 @File    : backup_recovery.py
 @Software: PyCharm
 '''
+
 from MySQLAutoPlatform.public.remote_cmd import RunCommand
-from base_module import MySQLOperations
+from base_module import *
 
 pt_xtrabackup = "/usr/bin/innobackupex"
 
@@ -40,17 +41,17 @@ class DbBackup(object):
         bakup_path = '/'.join(self.target_dir.split('/')[:-1])
         check_path = remote_run_com.check_path(bakup_path)
         if check_path:
-            command = "{0} --defaults-file={1} --user={2} --password='{3}' --host=localhost --port={4} --no-timestamp --parallel=4 --throttle=500 --use-memory=2GB --stream=xbstream ./ > {5}".format(
-                pt_xtrabackup, self.default_file, self.user, self.passwd, self.port, self.target_dir)
-            print command
-            reslut = remote_run_com.remote_cmd_err(command)
-            last_line = reslut[-1]
             try:
+                command = "{0} --defaults-file={1} --user={2} --password='{3}' --host=localhost --port={4} --no-timestamp --parallel=4 --throttle=500 --use-memory=2GB --stream=xbstream ./ > {5}".format(
+                    pt_xtrabackup, self.default_file, self.user, self.passwd, self.port, self.target_dir)
+                print command
+                reslut = remote_run_com.remote_cmd_err(command)
+                last_line = reslut[-1]
                 if 'completed OK!' in last_line:
                     return True
                 else:
                     return False
-            except Exception,BackupError:
+            except Exception, BackupError:
                 print BackupError
                 return BackupError
         else:
@@ -71,11 +72,73 @@ class BinlogBackup(object):
         command = ''
 
 
-# 恢复模块
 class DbRecovery(object):
-    #启动文件、配置文件、数据文件路径、远程IP、账号、密码
-    def __init__(self):
-        pass
+    """database recovery module"""
 
-    def remote_recovery(self):
-        pass
+    # 启动文件、配置文件、数据文件路径、远程IP、账号、密码
+    def __init__(self, source_url, db_conf):
+        self.source_url = source_url
+        self.db_conf = db_conf
+
+    def remote_recovery(self, rem_conn_par):
+        remote_run_com = RunCommand(rem_conn_par)
+        db_control = MySQLControl(self.db_conf['mysqld_dir'], rem_conn_par)
+        db_instance = MySQLOperations(db_conf['db_host'], db_conf['db_port'], db_conf['db_user'], db_conf['db_passwd'])
+
+
+        if not db_instance.db_connect():
+            pass
+        else:
+            db_instance.disconnect()
+            result = db_control.stop_db()
+
+            if not result:
+                print "stop db is faild!"
+                return False
+
+        """ clear data dir"""
+        cmd = "rm -rf %s*" % db_conf['data_dir']
+        print cmd
+        remote_run_com.remote_cmd(cmd)
+        """Download the backup file"""
+        # cmd = self.source_url + ' -C ' + self.db_conf['data_dir']
+        # get_bak_file = remote_run_com.remote_cmd(cmd)
+        get_bak_file = True
+        if get_bak_file:
+            cmd = "xbstream -x -v < %s%s -C  %s" % (
+                self.db_conf['data_dir'], self.source_url.split('/')[-1], self.db_conf['data_dir'])
+            print cmd
+            copy_bak = remote_run_com.remote_cmd(cmd)
+            if not copy_bak:
+                try:
+                    cmd = "%s --defalults_file=%s  --apply-log %s" % (
+                        pt_xtrabackup, self.db_conf['cnf_dir'], self.db_conf[
+                            'data_dir'])
+                    print cmd
+                    apply_log = remote_run_com.remote_cmd_err(cmd)
+                    last_line = apply_log[-1]
+
+                    if 'completed OK!' in last_line:
+                        cmd = "chown -R mysql:mysql %s" % self.db_conf['data_dir']
+                        chown_status = remote_run_com.remote_cmd(cmd)
+
+                        result = db_control.start_db()
+                        if result:
+                            return True
+                        return False
+                    else:
+                        return False
+                except Exception, RecoveryError:
+                    print RecoveryError
+                    return RecoveryError
+
+
+if __name__ == '__main__':
+    rem_conn_par = {"ip": "172.19.22.202", "user": "root", "password": "123456",}
+    db_conf = {"db_host": "172.19.22.202", "db_port": "3306", "db_user": "root", "db_passwd": "123456",
+               "cnf_dir": "/etc/my.cnf",
+               "mysqld_dir": "/etc/init.d/mysql",
+               "data_dir": "/data/mysql/mysql3306/data/"}
+    phy_bak = DbRecovery('/test_1', db_conf)
+    result = phy_bak.remote_recovery(rem_conn_par)
+    print result
--- mysql/base_module.py
+++ mysql/base_module.py
@@ -2,11 +2,12 @@
 # _*_ coding=utf-8 _*_
 
 import pymysql
+from MySQLAutoPlatform.public.remote_cmd import RunCommand
 
 #数据库操作命令
 class MySQLOperations(object):
-    def __init__(self, db_host, db_port, db_user, db_passwd, db_name='mysql', charset='utf8'):
-        self.conn_conf = {'host': db_host, 'port': db_port, 'user': db_user, 'password': db_passwd, 'db': db_name,
+    def __init__(self, db_host, db_port, db_user, db_passwd, db_name='information_schema', charset='utf8'):
+        self.conn_conf = {'host': db_host, 'port': int(db_port), 'user': db_user, 'password': db_passwd, 'db': db_name,
                           'charset': charset, 'cursorclass': pymysql.cursors.DictCursor,
                           }
 
@@ -17,7 +18,7 @@ class MySQLOperations(object):
             return True
         except Exception, ConnectionError:
             print (ConnectionError)
-            return ConnectionError
+            return False
 
     #关闭数据库连接
     def disconnect(self):
@@ -29,6 +30,7 @@ class MySQLOperations(object):
     #执行sql命令
     def exec_sql(self, sql):
         self.cursor = self.connection.cursor()
+        exec_sql = self.cursor.execute(sql)
         exec_sql = self.cursor.execute(sql)
         self.connection.commit()
         return exec_sql
@@ -47,12 +49,47 @@ class MySQLOperations(object):
         result = self.cursor.fetchall()
         return result
 
-    def shutdown(self):
-        pass
+
+
+
+class MySQLControl(object):
+    def __init__(self,mysqld_dir,rem_conn_par):
+        self.mysqld_dir=mysqld_dir
+        self.exec_cmd=RunCommand(rem_conn_par)
+
+
+    def start_db(self):
+        cmd='%s start' % self.mysqld_dir
+        result=self.exec_cmd.remote_cmd(cmd)
+        print result
+        if 'OK' in result[-1]:
+            print 'True'
+            return True
+        print 'False'
+        return False
+
+
+    def stop_db(self):
+        cmd='%s stop' % self.mysqld_dir
+        print cmd
+        result=self.exec_cmd.remote_cmd(cmd)
+        if 'OK' in result[-1]:
+            return True
+        return False
+
 
 if __name__ == '__main__':
+    rem_conn_par = {"ip": "172.19.22.202", "user": "root", "password": "123456",}
+    a=MySQLControl('/etc/init.d/mysql',rem_conn_par)
+    a.stop_db()
+
+
+    """
     a=MySQLOperations('172.19.22.201',3306,'root','123456')
     a.db_connect()
     result=a.fetch_all('select user,host,authentication_string from mysql.user;')
     print (result)
     a.disconnect()
+"""
+
+
